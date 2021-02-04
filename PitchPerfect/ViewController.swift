875e6f090abd7f946a51ef0907a011cd3c137567
Ygor <ygor.maniz@gmail.com>
2021-02-04 14:45:19 -0300

    caveman debugging

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed!")
+    }
+    
 }
 
